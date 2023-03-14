fb3658d9b3db887350056058cf37ada506c2706c
Yusuf <baliyusuf675@gmail.com>
2023-03-14 20:19:40 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div>
       <AuthContextProvider>
         <AppRouter />
+        <ToastContainer />
       </AuthContextProvider>
     </div>
   );
